65fba2d39b04b574e3da73a284c4355f47836b73
JuanGrajales <inbox1.juan+github@gmail.com>
2021-03-06 12:40:36 -0500

    task 2 complete

--- react/nucampsite/src/components/CampsiteInfoComponent.js
+++ react/nucampsite/src/components/CampsiteInfoComponent.js
@@ -1,15 +1,28 @@
 import React, { Component } from "react";
+import { Card, CardImg, CardBody, CardTitle, CardText } from "reactstrap";
 
 class CampsiteInfoComponent extends Component {
+  renderCampsite(campsite) {
+    return (
+      <div className="col-md-5 m-1">
+        <Card>
+          <CardImg top src={campsite.image} alt={campsite.name} />
+          <CardBody>
+            <CardTitle>{campsite.name}</CardTitle>
+            <CardText>{campsite.description}</CardText>
+          </CardBody>
+        </Card>
+      </div>
+    );
+  }
+
   render() {
     if (this.props.campsite) {
       return (
-        <div className="row">
-          <h1>hi</h1>
-        </div>
+        <div className="row">{this.renderCampsite(this.props.campsite)}</div>
       );
     } else {
-      return <div>No campsite selected yet {this.props.campsite}</div>;
+      return <div>No campsite selected yet</div>;
     }
   }
 }
